bfd4badcf1e506b17eeeed069869fc0dba3eca8b
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2020-10-30 17:23:28 +0000

    Update sakito_server.c

--- sakito_server.c
+++ sakito_server.c
@@ -270,7 +270,7 @@ int terminate_client(char* const buf, size_t cmd_len, SOCKET client_socket) {
 func parse_cmd(char* const buf) {
 	// Function pointer array of each c2 command.
 	const func func_array[4] = { &client_cd, &terminate_client, &send_file, &recv_file };
-	// Array of strings to be parsed.
+	// Array of command strings to parse stdin with.
 	const char commands[4][10] = { "cd ", "exit", "upload ", "download " };
 
 	for (int i = 0; i < 5; i++) {
