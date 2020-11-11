1949ac3735d33ad1ff24f5e3c5aeb9b974502816
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2020-11-11 03:44:48 +0000

    Create sakito_tools.h

---
+++ sakito_tools.h
@@ -0,0 +1,50 @@
+/* 
+Coded by d4rkstat1c.
+Use educationally/legally.
+#GSH ;)
+*/
+#ifndef SAKITO_TOOLS_H
+
+#define SAKITO_TOOLS_H
+
+#define BUFLEN 8192
+
+// Default allocation for conns.clients (to repeat repititive calls to realloc/reduce computations).
+#define MEM_CHUNK 5
+
+ // Function to read/store stdin until \n is detected.
+size_t get_line(char* const buf) {
+	char c;
+	size_t cmd_len = 0;
+
+	buf[cmd_len++] = '0';
+	c = getchar();
+
+	while (c != '\n' && cmd_len < BUFLEN) {
+		buf[cmd_len++] = c;
+		c = getchar();
+	}
+
+	return cmd_len;
+}
+
+// Function to compare two strings (combined logic of strcmp and strncmp).
+int compare(const char* buf, const char* str) {
+	for (int j = 0; str[j] != '\0'; j++) {
+		if (str[j] != buf[j])
+			return 0;
+	}
+
+	return 1;
+}
+
+// Function to copy int bytes to new memory block/location to abide strict aliasing.
+inline uint32_t ntohl_conv(char* const buf) {
+	uint32_t new;
+	memcpy(&new, buf, sizeof(new));
+
+	// Return deserialized bytes.
+	return ntohl(new);
+}
+
+#endif
