d8f867ec42f211afd7ab5de9d3b176e55c2c0b80
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-07-11 06:02:22 +0000

    Update sakito_server_tools.h

--- sakito_server_tools.h
+++ sakito_server_tools.h
@@ -1,9 +1,13 @@
+/* 
+Coded by d4rkstat1c.
+Use educationally/legally.
+*/
 #ifndef SAKITO_SERVER_TOOLS_H
 #define SAKITO_SERVER_TOOLS_H
 
 #define FILE_NOT_FOUND 1
 
- // Function to read/store stdin in buffer until \n is detected.
+// Function to read/store stdin in buffer until \n is detected.
 size_t get_line(char* const buf) {
 	size_t cmd_len = 0;
 	char c;
@@ -12,6 +16,15 @@ size_t get_line(char* const buf) {
 		buf[cmd_len++] = c;
 
 	return cmd_len;
+}
+
+// Function to compare two strings (combined logic of strcmp and strncmp).
+int compare(const char* buf, const char* str) {
+	for (int j = 0; str[j] != '\0'; j++)
+		if (str[j] != buf[j])
+			return 0;
+
+	return 1;
 }
 
 // Function to list/print all available connections to stdout.
@@ -30,4 +43,6 @@ void list_connections(const Conn_map* conns) {
 		printf("No connected targets available.\n\n\n");
 	}
 }
+
+
 #endif
