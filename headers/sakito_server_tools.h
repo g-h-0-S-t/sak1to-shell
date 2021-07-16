eb35d6b2cc101e53913f6ed2dc04e7b42fd574bf
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-07-16 13:46:31 +0000

    Update sakito_server_tools.h

--- headers/sakito_server_tools.h
+++ headers/sakito_server_tools.h
@@ -7,83 +7,32 @@ Use educationally/legally.
 
 #define BACKGROUND_CLIENT -100
 #define FILE_NOT_FOUND 1
+#define MEM_CHUNK 5
 
-#if defined(_WIN32) || defined(_WIN64) || (defined(__CYGWIN__) && !defined(_WIN32))
-	// Typedef for function pointer.
-	typedef int (*server_func)(char*, size_t, SOCKET);
-
-	typedef struct {
-		// Client hostname.
-		char* host;
-
-		// Client socket.
-		SOCKET sock;
-
-	} Conn;
-
-	typedef struct {
-		// Mutex object for race condition checks.
-		HANDLE ghMutex;
-
-		// Server buffer.
-		char buf[BUFLEN + 1];
-
-		// Thread handle for handling execution/termination of accept_conns thread.
-		HANDLE acp_thread;
-
-		// Server socket for accepting connections.
-		SOCKET listen_socket;
-
-		// Array of Conn objects/structures.
-		Conn* clients;
-
-		// Memory blocks allocated.
-		size_t clients_alloc;
-
-		// Amount of memory used.
-		size_t clients_sz;
-
-	} Server_map;
-#elif defined(__linux__)
-	#define SOCKET_ERROR -1
-	// Typedef for function pointer for server functions.
-	typedef int (*server_func)(char*, size_t, int);
-
-	typedef struct {
-		// Client hostname.
-		char* host;
-
-		// Client socket.
-		int sock;
-
-	} Conn;
-
-	typedef struct {
-		// Server buffer.
-		char buf[BUFLEN + 1];
-
-		// Server socket for accepting connections.
-		int listen_socket;
-
-		// Pthread object for handling execution/termination of accept_conns thread.
-		pthread_t acp_thread;
-
-		// Flag for race condition checks.
-		int THRD_FLAG;
-
-		// Array of Conn objects/structures.
-		Conn* clients;
-
-		// Memory blocks allocated.
-		size_t clients_alloc;
-
-		// Amount of memory used.
-		size_t clients_sz;
-
-	} Server_map;
+#if OS_WIN
+	#include "sakito_swin_utils.h"
+#elif OS_LIN
+	#include "sakito_slin_utils.h"
 #endif
 
+// Typedef for function pointer for console functions.
 typedef void (*console_func)(Server_map *s_map);
+
+// Typedef for function pointer for server functions.
+typedef int (*server_func)(char*, size_t, SOCKET);
+
+
+// Function to validate client identifier prior to interaction.
+int validate_id(Server_map* s_map) {
+	int client_id;
+	client_id = atoi(s_map->buf+9);
+
+	if (!s_map->clients_sz || client_id < 0 || client_id > s_map->clients_sz - 1)
+		return FAILURE;
+	else
+		return client_id;
+}
+
 
 // Function to compare two strings (combined logic of strcmp and strncmp).
 int compare(const char* buf, const char* str) {
@@ -114,23 +63,6 @@ void* parse_cmd(char* const buf, size_t *cmd_len, int cmds_len, const char comma
 
 	// If no command was parsed: send/execute the command string on the client via _popen().
 	return default_func;
-}
-
-// Function to list/print all available connections to stdout.
-void list_connections(Server_map* s_map) {
-	printf("\n\n---------------------------\n");
-	printf("---  C0NNECTED TARGETS  ---\n");
-	printf("--     Hostname: ID      --\n");
-	printf("---------------------------\n\n");
-
-	if (s_map->clients_sz) {
-		for (size_t i = 0; i < s_map->clients_sz; i++)
-			printf("%s: %lu\n", s_map->clients[i].host, i);
-		printf("\n\n");
-	}
-	else {
-		printf("No connected targets available.\n\n\n");
-	}
 }
 
 #endif
