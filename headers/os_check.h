a2bca2eeb92793cd0943216334559612315d602f
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-07-22 08:12:59 +0000

    Update os_check.h

--- headers/os_check.h
+++ headers/os_check.h
@@ -2,9 +2,9 @@
 #define OS_CHECK_H
 
 #if defined(_WIN32) || defined(_WIN64) || (defined(__CYGWIN__) && !defined(_WIN32))
-	#define OS_WIN 1
+	#define OS_WIN
 #elif defined(__linux__)
-	#define OS_LIN 1
+	#define OS_LIN
 
 #endif
 
