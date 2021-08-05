1b9747cef09f6883f7809522705581270f6cfdb6
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-08-05 22:55:24 +0000

    Update nbo_encoding.h

--- lib/headers/nbo_encoding.h
+++ lib/headers/nbo_encoding.h
@@ -4,15 +4,15 @@
 #include "os_check.h"
 
 #if __BIG_ENDIAN__
-    #include <winsock2.h>
-    #define htonll(x) (x)
-    #define ntohll(x) (x)
+#include <winsock2.h>
+#define htonll(x) (x)
+#define ntohll(x) (x)
 #else
-    #ifdef OS_LIN
-        #include <arpa/inet.h>
-    #endif
-    #define htonll(x) ((((uint64_t)htonl(x&0xFFFFFFFF)) << 32) + htonl(x >> 32))
-    #define ntohll(x) ((((uint64_t)ntohl(x&0xFFFFFFFF)) << 32) + ntohl(x >> 32))
+#ifdef OS_LIN
+#include <arpa/inet.h>
+#endif
+#define htonll(x) ((((uint64_t)htonl(x&0xFFFFFFFF)) << 32) + htonl(x >> 32))
+#define ntohll(x) ((((uint64_t)ntohl(x&0xFFFFFFFF)) << 32) + ntohl(x >> 32))
 #endif
 
 #endif
