9680fa0a1691d682b8d13e874f8caadaff061e5a
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-08-05 22:59:22 +0000

    Create sakito_slin_types.h

---
+++ lib/headers/linux/sakito_slin_types.h
@@ -0,0 +1,54 @@
+#ifndef SAKITO_LINUX_TYPES_H
+#define SAKITO_LINUX_TYPES_H
+
+#include <pthread.h>
+
+#ifndef BUFLEN
+#define BUFLEN 8192
+#endif
+#define SOCKET int
+#define CONSOLE_FSTR "sak1to-console:~%s$ "
+#define INTERACT_FSTR "┌%d─%s\n└%s>"
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+#define _FILE_OFFSET_BITS 64
+#define INVALID_FILE -1
+#define READ 1
+#define WRITE 0
+
+typedef struct {
+	// Client hostname.
+	char* host;
+
+	// Client socket.
+	SOCKET sock;
+
+} Conn;
+
+typedef struct {
+	// Server buffer.
+	char buf[BUFLEN + 9]; // BUFLEN + space for 'command code' + "cmd /C " + '\0'
+
+	// Server socket for accepting connections.
+	int listen_socket;
+
+	// Pthread object for handling execution/termination of accept_conns thread.
+	pthread_t acp_thread;
+
+	// Flag for race condition checks.
+	int THRD_FLAG;
+
+	// Array of Conn objects/structures.
+	Conn* clients;
+
+	// Memory blocks allocated.
+	size_t clients_alloc;
+
+	// Amount of memory used.
+	size_t clients_sz;
+
+} Server_map;
+
+typedef int s_file;
+
+#endif
