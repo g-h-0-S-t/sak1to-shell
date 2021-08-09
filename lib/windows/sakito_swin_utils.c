cb9ff1cdb8f933dd0b8eb3cc43b4663b9ebebeea
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-08-09 04:43:11 +0000

    Update sakito_swin_utils.c

--- lib/windows/sakito_swin_utils.c
+++ lib/windows/sakito_swin_utils.c
@@ -19,7 +19,6 @@ Use educationally/legally.
 void bind_socket(const SOCKET listen_socket);
 void s_accept_conns(Server_map* const s_map);
 
-
 /*
 Below contains windows specific sakito API functions.
 */
